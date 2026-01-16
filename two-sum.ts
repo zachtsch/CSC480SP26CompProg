3def39797549f37d9b5642980e8796724518e6cc
zachtsch <zachary.tschetter@usd.edu>
2026-01-16 11:28:11 -0600

    optimize two-sum

--- two-sum.ts
+++ two-sum.ts
@@ -1,10 +1,22 @@
 //    !  is the non-null assertion operator
+// naive O(n^2)
+// dictionary O(n)  linear
+
+//node+ts install instructions
 function twoSum(nums: number[], target: number): number[] {
+    const dict : Record<number,number> = {}
+    for(let i = 0; i < nums.length; i++)
+        dict[nums[i]] = i
     for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++){
-            if(nums[i]+nums[j]==target) return [i,j]
-        }
+        const temp = target-nums[i];
+        if(temp in dict && dict[temp]!=i)
+            return [i,dict[temp]]
     }
+    // for(let i = 0; i < nums.length; i++){
+    //     for(let j = i+1; j < nums.length; j++){
+    //         if(nums[i]+nums[j]==target) return [i,j]
+    //     }
+    // }
     throw Error('this shouldnt happen')
 };
 
