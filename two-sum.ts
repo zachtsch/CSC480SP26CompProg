827fe6dfd0a270334fa4a34450a9811e45a64dad
zachtsch <zachary.tschetter@usd.edu>
2026-01-16 11:22:38 -0600

    two-sum + config

--- two-sum.ts
+++ two-sum.ts
@@ -1,1 +1,11 @@
-//hello
+//    !  is the non-null assertion operator
+function twoSum(nums: number[], target: number): number[] {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i]+nums[j]==target) return [i,j]
+        }
+    }
+    throw Error('this shouldnt happen')
+};
+
+console.log(twoSum([2,7,11,15], 9))
