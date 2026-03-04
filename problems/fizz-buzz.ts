6acc77e21e4b791db54ad434f97f5829be4e9eb6
zachtsch <zachary.tschetter@usd.edu>
2026-03-04 11:19:00 -0600

    added 3 probs

---
+++ problems/fizz-buzz.ts
@@ -0,0 +1,10 @@
+function fizzBuzz(n: number): string[] {
+    const ans: string[] = []
+    for(let i = 1; i <= n; i++){
+        if(i % 3 == 0 && i%5==0) ans.push('FizzBuzz')
+        else if(i % 3 == 0)      ans.push('Fizz')
+        else if(i % 5 == 0)      ans.push('Buzz')
+        else                     ans.push(i+'')
+    }
+    return ans;
+};
